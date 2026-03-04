9623205692da0c43da6e763e3578b008f4334691
user.name <delvalle.fernando.d@gmail.com>
2026-03-04 19:51:14 -0300

    arreglos

--- Controller/contactController.php
+++ Controller/contactController.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '..Model/cors.php';
-require_once '..Model/connection.php';
-require_once '..Model/contactModel.php';
+require_once '../Model/cors.php';
+require_once '../Model/connection.php';
+require_once '../Model/contactModel.php';
 
 header('Content-Type: application/json');
 
